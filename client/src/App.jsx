d1648379d1959df220b01aecc4d833b4e5c823e9
doudaoud <boudaoudreda1@gmail.com>
2025-09-28 12:01:31 +0100

    cree la structure de front pour ce projet

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Message from './Components/Message'
+function App() {
+
+  return (
+    <>
+    <Message/>  
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Components/Message.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Message() {
+  return (
+    <div
+          style={{
+          color: "white",
+        backgroundColor: "rgb(16, 16, 16)",
+              display: "flex",
+              flexWrap:"nowrap",
+        justifyContent:"space-between"
+      }}
+    >
+      <span>
+        <span>EN | </span>
+        <span> Store Location</span>
+      </span>
+          <p>20% off with sms or email sign up</p>
+          <p>
+              
+          </p>
+    </div>
+  );
+}
